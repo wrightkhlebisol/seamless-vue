360943a01a1d8b08e887993d6ce0d9679649f782
wright <calebogundiya@gmail.com>
2020-06-11 18:58:45 +0100

    Initialised Tailwind and configured laravel-mix

---
+++ resources/views/employee.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>Laravel</title>
+
+		<link rel="stylesheet" href="{{ asset('css/main.css') }}">
+		
+	</head>
+	<body>
+		<div class="flex-center position-ref full-height" id="app">
+		   <div class="container mx-auto my-100 w-500">
+			<div class="container bg-white">Internia</div>
+			<div class="container bg-gray-200">
+				<div class="container">
+				<h2>employee</h2>
+				<div class="containe bg-white">
+					<p>All Employees</p>
+				</div>
+				</div>
+				<div class="container bg-yellow-500">add employee</div>
+			</div>
+			</div>
+		</div>
+	</body>
+</html>
