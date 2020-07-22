7865081209a15fdcf64c0f779d0e9aeb17bd34e6
wright <calebogundiya@gmail.com>
2020-07-22 22:54:05 +0100

    Setup schema for automatic https redirects

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,9 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         //
-        if(env('APP_ENV') !== 'local'){
+        if (env('APP_ENV') !== 'local') {
             \URL::forceScheme('https');
         }
     }
-
 }
